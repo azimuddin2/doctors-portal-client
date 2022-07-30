9f20d0ce810c0481752894857d07628d3279dadf
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-07-30 11:42:05 +0600

    added is services component

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -30,7 +30,7 @@ const Info = () => {
     ]
 
     return (
-        <div className='my-6 lg:my-20 px-8 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4'>
+        <div className='my-6 lg:my-20 px-8 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
             {
                 information.map(singleInfo => <InfoCard
                     key={singleInfo.id}
---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
+import Service from './Service';
+
+const Services = () => {
+    const services = [
+        {
+            _id: 1,
+            img: fluoride,
+            name: 'Fluoride Treatment',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        },
+        {
+            _id: 2,
+            img: cavity,
+            name: 'Cavity Filling',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        },
+        {
+            _id: 3,
+            img: whitening,
+            name: 'Teeth Whitening',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        },
+    ]
+
+    return (
+        <div>
+            <div className='text-center mb-12 mt-14'>
+                <h2 className='text-secondary font-bold mb-2'>OUR SERVICES</h2>
+                <h1 className='text-4xl text-accent'>Services We Provide</h1>
+            </div>
+            <div className='px-8 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
